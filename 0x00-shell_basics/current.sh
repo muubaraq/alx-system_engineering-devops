cd36223fc9a7332c75e862feead1861015b3a734
Mubaraq <58139049+muubaraq@users.noreply.github.com>
2022-03-02 12:54:28 +0100

    Delete 0x00-shell_basics directory

--- 0x00-shell_basics/current.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# i am a comment
-STRING="/home/vagrant/alx-system_engineering-devops/alx-system_engineering-devops/0x00-shell_basics/0-current_working_directory
-"
-echo STRING
-chmod u+x current.sh
