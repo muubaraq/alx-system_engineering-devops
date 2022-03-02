bec0e0e8440a873bc68b669f1d405814736ab40e
muubaraq <sixthlogic@gmail.com>
2022-03-02 13:16:18 +0100

    printing

--- 0x00-shell_basics/directory.sh
+++ 0x00-shell_basics/directory.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/bash
 # i am a comment
 STRING="/home/vagrant/alx-system_engineering-devops/alx-system_engineering-devops/0x00-shell_basics/0-current_working_directory"
 echo $STRING
