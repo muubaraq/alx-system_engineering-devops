dc91ea6594f9ae96ff48f302f5aec6be1c891d69
muubaraq <sixthlogic@gmail.com>
2022-03-02 12:48:00 +0100

    ..

--- 0x00-shell_basics/current.sh
+++ 0x00-shell_basics/current.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-/c/Users/Mr payne/Desktop/devps/alx-system_engineering-devops/0x00-shell_basics
-chmod u+x current.sh
+# i am a comment
+STRING="/home/vagrant/alx-system_engineering-devops/alx-system_engineering-devops/0x00-shell_basics/0-current_working_directory
+"
+echo STRING
