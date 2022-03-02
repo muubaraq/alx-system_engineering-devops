5e6d9c66a3f744ba3a63778cd6c2918bd35f338d
muubaraq <sixthlogic@gmail.com>
2022-03-02 13:03:14 +0100

    current directory

---
+++ 0x00-shell_basics/directory.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# i am a comment
+STRING="/home/vagrant/alx-system_engineering-devops/alx-system_engineering-devops/0x00-shell_basics/0-current_working_directory"
+echo $STRING
+chmod u+x directory.sh
